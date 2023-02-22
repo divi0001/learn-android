7f0687d6efa815497afe53b2cea14e11435b01a0
BuildTools <unconfigured@null.spigotmc.org>
2023-02-22 19:59:20 +0100

    simple registration form

--- app/src/main/java/de/example/helloworld/MainActivity.java
+++ app/src/main/java/de/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package de.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO: do x
+        //this will pop up in TODO, to remind of tasks
+        //go to Build -> Make Project might fix warnings
+        //if android studio bugs out: Go to File -> invalidate caches/Restart...
+
+
+        //Button btn = findViewById(R.id.button);
+
+
+    }
+
+    public void onBtnClick(View view) {
+        EditText editFName = findViewById(R.id.inputFirstName);
+        EditText editLName = findViewById(R.id.inputLastName);
+        EditText editEmail = findViewById(R.id.inputEmail);
+
+        String fname = editFName.getText().toString();
+        String lname = editLName.getText().toString();
+        String email = editEmail.getText().toString();
+
+        TextView txtFName = findViewById(R.id.viewFName);
+        TextView txtLName = findViewById(R.id.viewLName);
+        TextView txtEmail = findViewById(R.id.viewEmail);
+
+        txtFName.setText("First Name: "+fname);
+        txtLName.setText("Last Name: "+lname);
+        txtEmail.setText("E-mail: "+email);
     }
 }
